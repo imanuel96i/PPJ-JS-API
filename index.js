78eb694810dc010267daa6f2e1b6dd5d15ca3c90
Manuel Vidal <imanuel96i@gmail.com>
2022-11-30 11:26:58 -0300

    Se añadio API Categories

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { ListProduct, FindProduct, DeleteProduct, NewProduct } = require('./src/RestProducts')
+const { ListCategories, FindCategorie, DeleteCategorie, NewCategorie} = require('./src/RestCategories')
 
 /*
  * @author Manuel Vidal García
@@ -13,7 +14,7 @@ const logger = require('./logger')
 
 app.use(express.json())
 
-//Muestra por consola el consumo de la API
+//Muestra por consola el consumo de la Api
 app.use(logger)
 
 //Pagina inicial Api
@@ -21,27 +22,54 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
 
-//Lista de todos los productos
+//!Api Listas
+//Listar productos
 app.get('/api/products', (request, response) => {
     ListProduct(response)
 })
 
-//Listar producto por ID
+//Listar categorias
+app.get('/api/categories', (request, response) => {
+    ListCategories(response)
+})
+
+//!Api Buscar
+//Buscar producto por id
 app.get('/api/products/:id', (request, response) => {
     const id = parseInt(request.params.id)
     FindProduct(response, id)
 })
 
-//Elimina productos por ID
+//Buscar categoria por id
+app.get('/api/categories/:id', (request, response) => {
+    const id = parseInt(request.params.id)
+    FindCategorie(response, id)
+})
+
+//!Api Eliminar
+//Eliminar productos por id
 app.delete('/api/products/:id', (request, response) => {
     const id = parseInt(request.params.id)
     DeleteProduct(response,id)
 })
 
+//Eliminar categorias por id
+app.delete('/api/categories/:id', (request, response) => {
+    const id = parseInt(request.params.id)
+    DeleteCategorie(response,id)
+})
+
+//!Api Creación
 //Crea un nuevo producto
 app.post('/api/products', (request, response) => {
     const produ = request.body
     NewProduct(response, produ)
+})
+
+//Crea una nueva categoria
+app.post('/api/categories', (request, response) => {
+    const cate = request.body
+    NewCategorie(response, cate)
 })
 
 //Muestra error al entrar a un path no existente
---
+++ src/RestCategories.js
@@ -0,0 +1,44 @@
+let { categories } = require('./data')
+
+//Listar categorias
+const ListCategories = (res) => {
+    categories ? res.json(categories) : res.status(404).json({'Error': 'Hubo un error en el consumo de api'}).end()
+}
+
+//Buscar categoria por id
+const FindCategorie = (res, id) => {
+    const categorie = categories.find(pro => pro.id === id)
+    categorie ? res.json(categorie) : res.status(400).json({'Error': 'No se ha encontrado la categoria o hubo un error'}).end()
+}
+
+//Eliminar categoria por id
+const DeleteCategorie = (res, id) => {
+    const categorie = categories.find(pro => pro.id === id)
+    if (categorie) {
+        categories = categories.filter(pro => pro.id !== id)
+        res.status(200).json({ 'Mensaje': 'Se ha eliminado la categoria' })
+    } else {
+        res.status(400).json({ 'Error': 'Hubo un error en la eliminación de la categoria' }).end()
+    }
+}
+
+//Crear nueva categoria
+const NewCategorie = (res, body) => {
+    if (!body || !body.title || !body.img) {
+        return res.status(400).json({ error: 'Ocurrio un problema, faltan datos o existe un problema en la api' }).end()
+    }
+    
+    let ids = categories.map(pro => pro.id)
+    let maxId = Math.max(...ids)
+
+    let newCategorie = {
+        id: maxId + 1,
+        title: body.title,
+        img: body.img
+    }
+
+    categories = [...categories,newCategorie]
+    res.json(newCategorie)
+}
+
+module.exports = { ListCategories, FindCategorie, DeleteCategorie, NewCategorie}
