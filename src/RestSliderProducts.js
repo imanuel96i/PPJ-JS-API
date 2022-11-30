bbc8449fb4e087e2b534b7f2ac71eb020db39371
Manuel Vidal <imanuel96i@gmail.com>
2022-11-30 12:02:38 -0300

    Correccion de mensajes

--- src/RestSliderProducts.js
+++ src/RestSliderProducts.js
@@ -8,7 +8,7 @@ const ListSlider = (res) => {
 //Busca un producto del slider por id
 const FindSlider = (res, id) => {
     const slider = productsSlider.find(pro => pro.id === id)
-    slider ? res.json(slider) : res.status(400).json({'Error': 'No se ha encontrado la categoria o hubo un error'}).end()
+    slider ? res.json(slider) : res.status(400).json({'Error': 'No se ha encontrado el producto del slider o hubo un error'}).end()
 }
 
 //Elimina un producto del slider por id
@@ -16,15 +16,15 @@ const DeleteSlider = (res, id) => {
     const slider = productsSlider.find(pro => pro.id === id)
     if (slider) {
         productsSlider = productsSlider.filter(pro => pro.id !== id)
-        res.status(200).json({ 'Mensaje': 'Se ha eliminado la categoria' })
+        res.status(200).json({ 'Mensaje': 'Se ha eliminado el producto del slider' })
     } else {
-        res.status(400).json({ 'Error': 'Hubo un error en la eliminación de la categoria' }).end()
+        res.status(400).json({ 'Error': 'Hubo un error en la eliminación del producto en el slider' }).end()
     }
 }
 
 //Añade un producto al slider
 const NewSlider = (res, body) => {
-    if (!body || !body.title || !body.img) {
+    if (!body || !body.title || !body.price || !body.img) {
         return res.status(400).json({ error: 'Ocurrio un problema, faltan datos o existe un problema en la api' }).end()
     }
     
@@ -34,6 +34,7 @@ const NewSlider = (res, body) => {
     let newSlider = {
         id: maxId + 1,
         title: body.title,
+        price: body.price,
         img: body.img
     }
 
