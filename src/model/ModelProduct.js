f8887caf15f727b2b142ad3ccc9bd632f06d7b12
Manuel Vidal <imanuel96i@gmail.com>
2022-12-14 13:15:17 -0300

    Se añadieron comentarios

--- src/model/ModelProduct.js
+++ src/model/ModelProduct.js
@@ -1,5 +1,12 @@
 const {model, Schema} = require('mongoose')
 
+/*
+ * @author Manuel Vidal García
+ * Contacto: mvidal@acl.cl
+ * Fecha creación: 14/12/2022
+ * Fecha ultima modificación: 14/12/2022
+*/
+
 const productSchema = new Schema({
     title: { type: String, required: true },
     price: { type: Number, required: true },
